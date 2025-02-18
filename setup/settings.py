a21b4d86f5ff39f64ff651073ee918326e947674
Arthur Macedo <arthurmmaduro@gmail.com>
2025-02-18 19:50:21 -0300

    configração do ALLOWED_HOSTS

--- setup/settings.py
+++ setup/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = True
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
